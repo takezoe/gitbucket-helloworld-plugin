93c57addf48d25d63cee237cdf20ae1476f4af56
Naoki Takezoe <takezoe@gmail.com>
2018-06-15 19:06:10 +0900

    xx

--- src/main/scala/Plugin.scala
+++ src/main/scala/Plugin.scala
@@ -5,7 +5,10 @@ class Plugin extends gitbucket.core.plugin.Plugin {
   override val pluginId: String = "helloworld"
   override val pluginName: String = "HelloWorld Plugin"
   override val description: String = "First example of GitBucket plug-in"
-  override val versions: List[Version] = List(new Version("1.0.0"))
+  override val versions: List[Version] = List(
+    new Version("1.0.0"),
+    new Version("1.0.1")
+  )
 
   override val controllers = Seq(
     "/*" -> new HelloWorldController()
