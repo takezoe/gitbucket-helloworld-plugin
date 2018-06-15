24b15f74ea3f25be2feeb97706aed29fc88fec6b
Naoki Takezoe <takezoe@gmail.com>
2018-06-15 19:38:04 +0900

    test

--- src/main/scala/Plugin.scala
+++ src/main/scala/Plugin.scala
@@ -4,7 +4,7 @@ import io.github.gitbucket.solidbase.model.Version
 class Plugin extends gitbucket.core.plugin.Plugin {
   override val pluginId: String = "helloworld"
   override val pluginName: String = "HelloWorld Plugin"
-  override val description: String = "First example of GitBucket plug-in"
+  override val description: String = "Show simple message on GitBucket"
   override val versions: List[Version] = List(
     new Version("1.0.0"),
     new Version("1.0.1")
